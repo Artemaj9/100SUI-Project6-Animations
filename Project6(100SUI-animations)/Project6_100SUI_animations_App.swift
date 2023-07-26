3a3b61384979221982e3c7db499ba52eac7eef5e
Artem <moiseev@physics.msu.ru>
2023-07-26 21:41:06 +0300

    Initial Commit

---
+++ Project6(100SUI-animations)/Project6_100SUI_animations_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project6_100SUI_animations_App.swift
+//  Project6(100SUI-animations)
+//
+//  Created by Artem on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Project6_100SUI_animations_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
